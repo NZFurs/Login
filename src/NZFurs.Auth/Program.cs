d22ce0921feddb05bb0b9af4605558da535af7cc
TC Foxtaur <tcfox@tc.nz>
2018-10-15 00:03:24 +1300

    Moved logs and database to a more easily mountable location

--- src/NZFurs.Auth/Program.cs
+++ src/NZFurs.Auth/Program.cs
@@ -20,7 +20,7 @@ namespace NZFurs.Auth
                 .MinimumLevel.Override("System", LogEventLevel.Warning)
                 .MinimumLevel.Override("Microsoft.AspNetCore.Authentication", LogEventLevel.Information)
                 .Enrich.FromLogContext()
-                .WriteTo.File(@"identityserver4_log.txt")
+                .WriteTo.File(@"Data/Log/log.txt")
                 .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}", theme: AnsiConsoleTheme.Literate)
                 .CreateLogger();
 
