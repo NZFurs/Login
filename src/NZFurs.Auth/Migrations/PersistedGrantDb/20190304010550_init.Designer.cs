c21c53015952ce6fb839cbb41a1ce3ddd52fa3c2
Roman Vaughan <nzsmartie@gmail.com>
2019-03-04 14:08:36 +1300

    Re-add migrations with Postgres as a provider

--- src/NZFurs.Auth/Migrations/PersistedGrantDb/20190304010550_init.Designer.cs
+++ src/NZFurs.Auth/Migrations/PersistedGrantDb/20190304010550_init.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using IdentityServer4.EntityFramework.DbContexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace NZFurs.Auth.Migrations.PersistedGrantDb
 {
     [DbContext(typeof(PersistedGrantDbContext))]
-    partial class PersistedGrantDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190304010550_init")]
+    partial class init
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
