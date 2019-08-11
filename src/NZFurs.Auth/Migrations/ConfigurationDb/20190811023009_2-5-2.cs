baae7b8bcc44949249f364374e95ee6eea9ea7fa
TC Foxtaur <tcfox@tc.nz>
2019-08-11 14:38:16 +1200

    Update IdentityServer4 DbContexts to 2.5.2

---
+++ src/NZFurs.Auth/Migrations/ConfigurationDb/20190811023009_2-5-2.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NZFurs.Auth.Migrations.ConfigurationDb
+{
+    public partial class _252 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
